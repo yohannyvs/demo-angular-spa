5046c6eee35f49f3d5f7d742badeda628868614c
yohannyvs <yohannyvs@gmail.com>
2018-07-06 13:07:44 -0600

    Arreglo Buscar - Fin App

--- src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
+++ src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -20,8 +20,8 @@ export class HeroeTarjetaComponent implements OnInit {
   }
 
   verHeroe() {
-    //this.router.navigate( ['/heroe', this.index ] );
-    this.heroeSeleccionado.emit( this.index );
+    this.router.navigate( ['/heroe', this.index ] );
+    //this.heroeSeleccionado.emit( this.index );
   }
 
 }
--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -15,6 +15,7 @@ export class HeroesComponent implements OnInit {
 
   ngOnInit() {
     this.heroes = this._heroesService.getHeroes();
+    console.log( this.heroes );
   }
 
   verHeroe( idx:number ) {
